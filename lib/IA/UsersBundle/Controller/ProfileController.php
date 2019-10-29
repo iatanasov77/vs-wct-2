1d7684dfcd88876af4faa815be9c6b7b9114cff7
Ivan Atanasov (Accenture) <ivan.atanasov@accenture.com>
2019-10-29 14:09:13 +0200

    Commit

--- lib/IA/UsersBundle/Controller/ProfileController.php
+++ lib/IA/UsersBundle/Controller/ProfileController.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace IA\UsersBundle\Controller;
+<?php namespace IA\UsersBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,29 +7,27 @@ use IA\UsersBundle\Form\ProfileFormType;
 
 class ProfileController extends Controller
 {
-    public function showAction(Request $request)
+    public function showAction( Request $request )
     {
         $form = $this->createForm( ProfileFormType::class, $this->getUser()->getUserInfo() );
         
         /*
          * Fetch Available Packages
          */
-        $pr = $this->getDoctrine()->getRepository('IAPaidMembershipBundle:Package');
+        $pr = $this->getDoctrine()->getRepository( 'IAPaidMembershipBundle:Package' );
 //        $search = array('enabled' => 1);
 //        $order = array();
 //        $packages = $pr->findBy($search, $order);
         $packages = $pr->findAll();
         
-        $paymentMethods = $this->container->getParameter('ia_payment.methods');
+        $paymentMethods = $this->container->getParameter( 'ia_payment.methods' );
         
-        $tplVars = array();
-        
-        $tplVars = array(
+        return $this->render( 'IAUsersBundle:Profile:show.html.twig', [
+            'subscription'  => $this->getUser()->getSubscription() ,
             'form'          => $form->createView(),
             'packages'      => $packages,
             'paymentMethods'=> $paymentMethods
-        );
-        return $this->render('IAUsersBundle:Profile:show.html.twig', $tplVars);
+        ]);
     }
     
 }
