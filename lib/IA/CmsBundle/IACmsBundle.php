65b7ed0bbb06cfa919a3bd412adace1b8eed338e
Ivan Atanasov <i.atanasov77@gmail.com>
2020-01-06 06:23:54 +0000

    Commit

--- lib/IA/CmsBundle/IACmsBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace IA\CmsBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class IACmsBundle extends Bundle
-{
-}
--- lib/IA/TaxonomyBundle/IATaxonomyBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace IA\TaxonomyBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class IATaxonomyBundle extends Bundle
-{
-}
