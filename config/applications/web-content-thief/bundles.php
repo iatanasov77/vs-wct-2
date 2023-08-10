913ce683b7baaa33ed5d87e09ac14e3b74466345
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-08-10 08:05:43 +0000

    Load and  MonologBundle for Loggingg.

--- config/applications/web-content-thief/bundles.php
+++ config/applications/web-content-thief/bundles.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
